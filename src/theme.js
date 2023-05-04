ab50b4d7b9d3a2a29743491ebc6ae1a69b74d4c2
Mohanad Inairat <mohanad@menatal.com>
2023-05-04 21:05:39 +0300

    first commit

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+import { red, grey } from '@mui/material/colors';
+import { createTheme } from '@mui/material/styles';
+
+// A custom theme for this app
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#ea6241',
+        },
+        secondary: {
+            main: grey[800],
+        },
+        error: {
+            main: red.A400,
+        },
+    },
+});
+
+export default theme;
