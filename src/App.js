330e5564fc39bd0647087d8f17cf33084d179037
Mohanad Inairat <mohanad@menatal.com>
2023-05-06 19:49:36 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -2,36 +2,25 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import { useSettings } from './@core/hooks/useSettings';
 import MainLayout from './@core/layouts/MainLayout';
-
-function Copyright() {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
+import { navigation } from './routes';
 
 export default function App() {
     const { settings } = useSettings()
+
+    const router = createBrowserRouter(navigation);
 
     return (
         <MainLayout
             settings={settings}
             contentHeightFixed="true"
         >
-            <Box>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    Material UI Create React App example
-                </Typography>
-                <Copyright />
-            </Box>
+            <RouterProvider router={router} />
         </MainLayout>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,19 @@
-const navigation = () => {
-    return [
-        {
-            icon: 'mdi:home-outline',
-            title: 'Document Control',
-            children: [
-                {
-                    icon: 'mdi:chart-donut',
-                    title: 'Setup',
-                    path: '/document-control/setup'
-                }
-            ]
-        }
-    ]
-}
+export const navigation = [
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+    {
+        path: "/documentcontrol",
+        icon: 'mdi:home-outline',
+        title: 'Document Control',
+        children: [
+            {
+                icon: 'mdi:chart-donut',
+                title: 'Setup',
+                path: '/documentcontrol/setupme',
+                element: <div>document setup!</div>
+            }
+        ]
+    }
+];
