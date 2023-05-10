2f8e480500d5300d79ec131789bda397c2b8b24d
Mohanad Inairat <mohanad@menatal.com>
2023-05-10 23:04:57 +0300

    what to do 2

---
+++ src/modules/document-control/pages/Setup/Components/Types/Subject/Body/SubjectBody.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+export default function SubjectBody() {
+    useEffect(() => {
+        // set is loading
+        // load data
+    }, []);
+
+    return (
+        <>
+            <h1>SubjectBody</h1>
+        </>
+    );
+}
