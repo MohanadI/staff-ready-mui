21a6badfce7b866962751373307658431bf5adaa
Mohanad Inairat <mohanad@menatal.com>
2023-05-16 22:33:35 +0300

    document and revision

--- src/modules/document-control/pages/Setup/Components/Types/DocumentsTemplate.js
+++ src/modules/document-control/pages/Setup/Components/Types/DocumentsTemplate.js
@@ -1,9 +1,107 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from "react";
+
+import Context from "../../Context";
+import AccordionComponent from "../../../../../../@core/components/accordion";
+
 
 export default function DocumentsTemplate() {
-    return (
-        <>
-            DocumentsTemplate
-        </>
-    );
+  const [isLoading, setIsLoading] = useState(false);
+  const [subjectID, setSubjectID] = useState(null);
+  const { setupPageData } = useContext(Context);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setSubjectID(setupPageData?.selectedNode?.value);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, [setupPageData]);
+
+  const panels = [
+    {
+      key: "document_history",
+      title: "Document History",
+      iconApi: "",
+      description: "ss",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "revision_history",
+      title: "Revision History",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "document_properties",
+      title: "Document Properties",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "reviewers",
+      title: "Reviewers",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "final_reviewers",
+      title: "Final Reviewers",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "approvers",
+      title: "Approvers",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "acknowledgment_list",
+      title: "Acknowledgment List",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "messages",
+      title: "Messages",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "assigned_locations",
+      title: "Assigned Locations",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "associated_documents",
+      title: "Associated Documents",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+    {
+      key: "publish_history",
+      title: "Publish History",
+      iconApi: "",
+      description: "",
+      body: <h1>asdasd</h1>,
+    },
+  ];
+
+  return (
+    <>
+      {panels.map((item) => (
+        <AccordionComponent key={item.key} item={item} isLoading={isLoading} />
+      ))}
+    </>
+  );
 }
--- src/modules/document-control/pages/Setup/Components/Types/RevisionTemplate.js
+++ src/modules/document-control/pages/Setup/Components/Types/RevisionTemplate.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from "react";
+
+import Context from "../../Context";
+import AccordionComponent from "../../../../../../@core/components/accordion";
+
 
 export default function RevisionTemplate() {
-    return (
-        <>
-            RevisionTemplate
-        </>
-    );
+  const [isLoading, setIsLoading] = useState(false);
+  const [subjectID, setSubjectID] = useState(null);
+  const { setupPageData } = useContext(Context);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setSubjectID(setupPageData?.selectedNode?.value);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, [setupPageData]);
+
+  const panels = [
+    {
+      key: "revision",
+      title: "Revision",
+      iconApi: "",
+      description: "ss",
+      body: <h1>asdasd</h1>,
+    },
+  ];
+
+  return (
+    <>
+      {panels.map((item) => (
+        <AccordionComponent key={item.key} item={item} isLoading={isLoading} />
+      ))}
+    </>
+  );
 }
